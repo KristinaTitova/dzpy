fcf8ae13637ec7b54ffd229d3b60532d23b8e9e1
KristinaTitova <kristytitova@mail.ru>
2022-11-20 10:18:53 +0300

    Домашка 2

--- dz2.py
+++ dz2.py
@@ -1,3 +1,5 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
 for x in [True,False]:
     for y in [True,False]:
         for z in [True,False]:
